ec8f1b8aa15f1cdf6bca65161edccc25b5df3962
Miroslav Tsarev <mtzarev@gmail.com>
2024-03-26 23:32:08 +0200

    Add files via upload

---
+++ Ex26.03.2024-Info.js
@@ -0,0 +1,14 @@
+function ex26032024(name, type, ambition, town, level) {
+    let personInfo = {};
+    personInfo.name = name;
+    personInfo.type = type;
+    personInfo.ambition = ambition;
+    personInfo.town = town;
+    personInfo.level = level
+
+    console.log(personInfo);
+}
+ex26032024("Miroslav Tzarev", 
+"Student",
+"Developer", "Plovdiv", "Fundamentals"
+);
